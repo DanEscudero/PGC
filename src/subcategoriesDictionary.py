bc6c881c7346dfe9f1432557842b64047188b969
DanEscudero <daniel.escudero05@gmail.com>
2020-02-27 10:47:40 -0300

    feat: add queries accumulator

--- src/subcategoriesDictionary.py
+++ src/subcategoriesDictionary.py
@@ -3,7 +3,8 @@ from util import format_field, pp_json, clear_category_name, list_subcategories
 
 # Returns subcategories as dictionary.
 # TODO: add count of subcategories as a state, passed along with accumulated
-def recursive_subcategories(field, parent, accumulated, level):
+def r_subs(field, parent, accumulated, level):
+    global queries
     # Base case
     if (level == 0):
         # create entry as list (if it's not created yet)
@@ -21,17 +22,32 @@ def recursive_subcategories(field, parent, accumulated, level):
 
     # Recursive step
     subcategories = list_subcategories(field)
+    queries += 1
+
     for subcategory in subcategories:
-        accumulated[parent] = recursive_subcategories(subcategory,
-                                                      field, accumulated[parent], level-1)
+        search(subcategory, field, state, level-1)
+
+    if (len(subcategories)):
+        for subcategory in subcategories:
+            next = r_subs(subcategory,
+                          field, accumulated[parent], level-1)
+            accumulated[parent] = next
+    else:
+        r_subs('', field, state, 0)
 
     return accumulated
 
 
+# Number of queries q:
+# q <= Sum of i = 1 up to n of (i-1) ^ l = O(n^l)
+# where n is number of levels, and l is the maximum length of the list
+
 # Parameters
+queries = 0
 field = 'Neuroscience'
 entryName = 'Subcategories for ' + field
 levels = 2  # 5 takes way too long to answer
 
-subcategories = recursive_subcategories(field, entryName, dict(), levels)
+subcategories = r_subs(field, entryName, dict(), levels)
+print('Number of queries:', queries)
 pp_json(subcategories)
